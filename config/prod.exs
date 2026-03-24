4cca325ed9ef0e9b2f3e6b4d253047124959c5b7
Nixox <46408125+mbanixox@users.noreply.github.com>
2026-03-24 15:45:00 +0300

    Update prod environment configuration

--- config/prod.exs
+++ config/prod.exs
@@ -3,12 +3,12 @@ import Config
 # Force using SSL in production. This also sets the "strict-security-transport" header,
 # known as HSTS. If you have a health check endpoint, you may want to exclude it below.
 # Note `:force_ssl` is required to be set at compile-time.
-config :patakituo_backend, PatakituoBackendWeb.Endpoint,
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  exclude: [
-    # paths: ["/health"],
-    hosts: ["localhost", "127.0.0.1"]
-  ]
+config :patakituo_backend, PatakituoBackendWeb.Endpoint
+  # force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  # exclude: [
+  #   # paths: ["/health"],
+  #   hosts: ["localhost", "127.0.0.1", System.get_env("PHX_HOST") || "example.com"]
+  # ]
 
 # Do not print debug messages in production
 config :logger, level: :info
